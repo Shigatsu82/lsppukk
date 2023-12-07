c6727aecc5364e1e2d5ece1a9829c790d48addfb
Zulham Daninoor <zulhamnoor2@gmail.com>
2023-12-07 13:57:36 +0700

    add seri field

--- database/migrations/2023_12_07_004748_create_barang_table.php
+++ database/migrations/2023_12_07_004748_create_barang_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('barang', function (Blueprint $table) {
             $table->integer('id')->autoIncrement();
             $table->string('merk', 30);
+            $table->string('seri', 40);
             $table->text('spesifikasi');
             $table->smallInteger('stok');
             $table->smallInteger('kategori_id');
