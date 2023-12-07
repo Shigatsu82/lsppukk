fe7f4d394f476fe1b96413ae0dec26c87f118970
Zulham Daninoor <zulhamnoor2@gmail.com>
2023-12-07 09:55:25 +0700

    add controller kategori

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -3,15 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Kategori;
+use Illuminate\Support\Facades\DB;
 
 class KategoriController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    public function index(Request $request)
     {
-        //
+    /**
+    * Display a listing of the resource.
+    */
+
+    // $rsetKategori = Kategori::infoKategori();
+    // return $rsetKategori;
+
+        // $rsetKategori = DB::table('kategori')->select('id','kategori',DB::raw('ketKategori(jenis) as ketkategori'))->get();
+        // return $rsetKategori;
+
+        // return DB::table('kategori')->get();
+
+    // memanggil store procedure
+        // return DB::select('CALL getKategoriAll()');
+
+    // memanggil store procedure dengan view
+        // $rsetKategori = DB::select('CALL getKategoriAll()');
+        // return view('v_smartkoding.index',compact('rsetKategori'));
+
+    $rsetKategori = Kategori::getKategoriAll()->paginate(10);
+    return view('kategori.index',compact('rsetKategori'))
+        ->with('i', (request()->input('page', 1) - 1) * 10);
+    
+    // $rsetKategori = DB::table('kategori')->select('id','deskripsi',DB::raw('ketKategorik(kategori) as ketkategorik'))->paginate(10);
+    // return view('kategori.index',compact('rsetKategori'))
     }
 
     /**
@@ -19,7 +42,13 @@ class KategoriController extends Controller
      */
     public function create()
     {
-        //
+        $akategori = array('blank'=>'Pilih Kategori',
+                            'M'=>'Barang Modal',
+                            'A'=>'Alat',
+                            'BHP'=>'Bahan Habis Pakai',
+                            'BTHP'=>'Bahan Tidak Habis Pakai'
+                            );
+        return view('kategori.create',compact('akategori'));
     }
 
     /**
@@ -27,7 +56,22 @@ class KategoriController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // return $request->all();
+
+        $this->validate($request, [
+            'deskripsi'   => 'required',
+            'kategori'     => 'required | in:M,A,BHP,BTHP',
+        ]);
+
+
+        // create post
+        Kategori::create([
+            'deskripsi'  => $request->deskripsi,
+	        'kategori'   => $request->kategori,
+        ]);
+
+        //redirect to index
+        return redirect()->route('kategori.index')->with(['success' => 'Data Berhasil Disimpan!']);
     }
 
     /**
@@ -35,7 +79,17 @@ class KategoriController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $rsetKategori = Kategori::find($id);
+
+        // $rsetKategori = Kategori::select('id','deskripsi','kategori',
+        //     \DB::raw('(CASE
+        //         WHEN kategori = "M" THEN "Modal"
+        //         WHEN kategori = "A" THEN "Alat"
+        //         WHEN kategori = "BHP" THEN "Bahan Habis Pakai"
+        //         ELSE "Bahan Tidak Habis Pakai"
+        //         END) AS ketKategori'))->where('id', '=', $id);
+
+        return view('kategori.show', compact('rsetKategori'));
     }
 
     /**
@@ -43,22 +97,56 @@ class KategoriController extends Controller
      */
     public function edit(string $id)
     {
-        //
-    }
+    $akategori = Kategori::all();
+    $rsetKategori = Kategori::find($id);
 
+    return view('kategori.edit', compact('rsetKategori', 'akategori'));
+    }
     /**
      * Update the specified resource in storage.
      */
     public function update(Request $request, string $id)
     {
-        //
+        $this->validate($request, [
+            'deskripsi'   => 'required',
+            'kategori'     => 'required | in:M,A,BHP,BTHP',
+        ]);
+
+        $rsetKategori = Kategori::find($id);
+
+            //update post without image
+            $rsetKategori->update([
+                'deskripsi'  => $request->deskripsi,
+	            'kategori'   => $request->kategori,
+            ]);
+
+        // Redirect to the index page with a success message
+        return redirect()->route('kategori.index')->with(['success' => 'Data Berhasil Diubah!']);
     }
 
     /**
      * Remove the specified resource from storage.
      */
     public function destroy(string $id)
+
     {
-        //
+
+        //cek apakah kategori_id ada di tabel barang.kategori_id ?
+
+        if (DB::table('barang')->where('kategori_id', $id)->exists()){
+
+            return redirect()->route('kategori.index')->with(['Gagal' => 'Data Gagal Dihapus!']);
+
+
+        } else {
+
+            $rsetKategori = Kategori::find($id);
+
+            $rsetKategori->delete();
+
+            return redirect()->route('kategori.index')->with(['success' => 'Data Berhasil Dihapus!']);
+
+        }
+
     }
 }
