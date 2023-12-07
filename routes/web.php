180b95e7ecad60b73c2ecabfc39641d3e59caeed
Zulham Daninoor <zulhamnoor2@gmail.com>
2023-12-07 09:54:23 +0700

    add theme adn some migrations

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\BarangController;
+use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +19,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('login', [LoginController::class,'index'])->name('login')->middleware('guest');
+Route::post('login', [LoginController::class,'authenticate']);
+Route::post('logout', [LoginController::class,'logout']);
+
+Route::get('daftar', [RegisterController::class, 'create'])->name('daftar');
+Route::post('register', [RegisterController::class,'store'])->name('register');
+
+Route::middleware('auth')->group(function(){
+    Route::resource('barang', BarangController::class);
+    Route::resource('kategori', KategoriController::class);
 });
