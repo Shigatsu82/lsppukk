7533a2c76bcbe10ec3d0ba96c13432d907982733
Zulham Daninoor <zulhamnoor2@gmail.com>
2023-12-08 07:48:36 +0700

    change title

--- resources/views/layouts/adm-main.blade.php
+++ resources/views/layouts/adm-main.blade.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>GuruKoding</title>
+    <title>SIJA SYSTEM INVENTORY</title>
 
     <!-- Custom fonts for this template-->
     <link href="{!! asset('theme/vendor/fontawesome-free/css/all.min.css') !!}" rel="stylesheet" type="text/css">
